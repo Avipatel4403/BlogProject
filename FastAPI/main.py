69f3b9ae81fbad47a708927a1ed580f59d5c1cd0
Avipatel4403 <avipatel4403@gmail.com>
2023-08-07 20:25:24 -0400

    v1

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+    public: bool
+    published: bool
+
+
 @app.get("/")
 async def root():
-    return{"message": "Hello World"}
+    return {"message": "Hello World"}
+
+
+@app.post("/createposts")
+def create_post(post: Post):
+    print(post)
+    return {"data": "New post"}
